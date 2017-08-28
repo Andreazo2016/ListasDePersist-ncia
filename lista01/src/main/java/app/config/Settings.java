fea2d1150c5b14bbd73f4d96eaf28db44d35b92f
andreazo SIlva <andreazocoisas@gmail.com>
2017-08-27 23:20:09 -0300

    Alteração no caminho Para o arquivo CVS

--- lista01/src/main/java/app/config/Settings.java
+++ lista01/src/main/java/app/config/Settings.java
@@ -3,17 +3,17 @@ package app.config;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Path;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
 public class Settings {
 	public void CreateProperties() throws IOException{
-		Path p;
+		String caminhoRelativo = "/lista01/src/main/resources/Database/Contato.csv";
 		Properties pro = new Properties();
-		//String CominhoArquivo = Settings.class.getResourceAsStream("/Database/Contato.csv").toString() ;
-		String CominhoArquivo = "/home/andreazo/workspace/lista01/src/main/resources/Database/Contato.csv";
-		pro.setProperty("nameArquivo",CominhoArquivo);
+		String CaminhoArquivo = System.getProperty("user.home") + "/workspace" + caminhoRelativo;
+		pro.setProperty("nameArquivo",CaminhoArquivo);
 		pro.store(new FileOutputStream("config.properties"),null);
 		
 	}
